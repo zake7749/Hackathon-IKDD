14ba0ba947ea25b85da404a6c84fdaf8974c764f
nienzu <ibqqz0602@gmail.com>
2017-01-12 20:10:15 +0800

    fix the init step when changing player

--- ingan.py
+++ ingan.py
@@ -1,38 +1,46 @@
 import csv
-gameID = "init"
 playerID =""
 result = dict()
 score=0
-ssss = []
+gameID = "init"
 with open("shot_logs.csv", "r") as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter=',', quotechar='\"')
         csv_reader.next()
 	for row in csv_reader:
-            playerID = row[19]
-            status = row[3]
-            if(gameID != row[0]):
+            if (row[19] not in result):
+                if(gameID!= "init"):
+                    if(gameID != row[0]):
+                        result[playerID][status].append(score)
+
+                status = row[3]
                 gameID = row[0]
-                if(playerID not in result):
-                    result[playerID]=dict()
-                    result[playerID]["W"]=[]
-                    result[playerID]["L"]=[]
+                score=0
+                playerID = row[19]
+                result[playerID] = dict()
+                result[playerID]["W"]=[]
+                result[playerID]["L"]=[]
+            if(gameID != row[0]):
                 result[playerID][status].append(score)
                 score = 0
-            else :
-                if (row[19] not in result):
-                    diction[row[19]]= []
-                else:
-                    score += int(row[18])
+                playerID = row[19]
+                status = row[3]
+                gameID = row[0]
+            score += int(row[18])
         result_list = result.items()
         with open("quiz3.txt","w") as outputFile:
             for x in result_list:
                 losePoint = max(x[1]["L"])
+                if(x[0] == 'kyle korver'):
+                    print x
                 win = -1
+                minWin=111999
                 for eachWin in x[1]["W"]:
                     if(eachWin > losePoint):
                         win = eachWin
-                if(win != -1):
-                    outputFile.write(str(x[0])+" "+str(win)+"\n")
+                        if win < minWin:
+                            minWin = win
+                if( win != -1):
+                    outputFile.write(str(x[0])+" "+str(minWin)+"\n")
                 else:
                     outputFile.write(str(x[0])+" no\n")
 
